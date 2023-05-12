5a9bbbf367aea5e48f3c382eaea34f09ba762be3
grs-se <george_ricesmith@hotmail.com>
2023-05-12 21:00:46 +0100

    Add routing and multiple pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home';
+import { LoginComponent, RegisterComponent } from './account';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
